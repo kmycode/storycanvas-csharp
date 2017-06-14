d2beee2ac70bfdfe5fece54f53fe3c3322993a22
KMY <tt@kmycode.net>
2017-06-14 22:08:44 +0900

    [UWP] Xamarin.Forms対応

--- StoryCanvas.UWP/MainPage.xaml.cs
+++ StoryCanvas.UWP/MainPage.xaml.cs
@@ -31,11 +31,8 @@ namespace StoryCanvas.UWP
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
-    public sealed partial class MainPage : Page, IAuthBrowserProvider
+    public sealed partial class MainPage
 	{
-		private MainMode _beforeMainMode;
-		private bool _isStartPage = true;
-
 		public MainPage()
         {
 			AutofacUtil.OneDriveStorage = new OneDriveStorageModel();
@@ -48,121 +45,10 @@ namespace StoryCanvas.UWP
 			{
 				await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => message.Action());
 			});
+
             this.InitializeComponent();
-			this.DataContext = StoryViewModel.Default;
 
-			this._beforeMainMode = StoryViewModel.Default.MainMode;
-
-			this.RegisterMessages();
-
-			// 画面遷移
-			((StoryViewModel)this.DataContext).PropertyChanged += (sender, e) =>
-			{
-				// メインモードが切り替えられた時
-				if (e.PropertyName == "MainMode")
-				{
-					throw new NotImplementedException();
-					/*
-					// フライアウト対応
-					foreach (Flyout flyout in this.Flyouts.Items)
-					{
-						flyout.IsOpen = false;
-					}
-
-					// フライアウトを表示してもそれまでのデータが消えないよう、編集中のテキストボックスからフォーカスを外す
-					this.DetermineCurrentInputing();
-
-					// フライアウトを表示しても、それまでの編集画面が閉じないようにする
-					MainMode mode = ((StoryViewModel)this.DataContext).MainMode;
-					switch (mode)
-					{
-						// フライアウト
-						case MainMode.StoragePage:
-							((StoryViewModel)this.DataContext).MainMode = this._beforeMainMode;
-							this.StorageFlyout.IsOpen = true;
-							break;
-						case MainMode.NetworkPage:
-							((StoryViewModel)this.DataContext).MainMode = this._beforeMainMode;
-							this.NetworkFlyout.IsOpen = true;
-							break;
-						case MainMode.AboutPage:
-							((StoryViewModel)this.DataContext).MainMode = this._beforeMainMode;
-							this.AboutFlyout.IsOpen = true;
-							break;
-						default:
-							this._beforeMainMode = mode;
-
-							// スタートページが表示されていれば閉じる
-							if (this._isStartPage && mode != MainMode.StartPage)
-							{
-								this._isStartPage = false;
-								this.MainFrame.Content = new MainPage();
-							}
-
-							break;
-					}
-					*/
-				}
-			};
-
-			// Autofacに自身を登録
-			AutofacUtil.AuthBrowserProvider = this;
-
-			// UWPタイトルバーとか
-			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
-		}
-
-		public IAuthBrowser OpenBrowser()
-		{
-			throw new NotImplementedException();
-		}
-
-		private void RegisterMessages()
-		{
-			// ネットワーク選択画面を表示
-			Messenger.Default.Register<OpenNetworkChooseMessage>((message) =>
-			{
-				throw new NotImplementedException();
-			});
-
-			// ネットワーク受信画面を表示
-			Messenger.Default.Register<OpenNetworkReceiveMessage>((message) =>
-			{
-				throw new NotImplementedException();
-			});
-
-			// ネットワーク送信画面を表示
-			Messenger.Default.Register<OpenNetworkSendMessage>((message) =>
-			{
-				throw new NotImplementedException();
-			});
-
-			// シーン編集画面をモーダル表示
-			Messenger.Default.Register<StartEditSceneMessage>((message) =>
-			{
-				throw new NotImplementedException();
-				//view.SubViewClosed += (sender, e) => message.OnEditorClosed();
-				// ここで画面表示
-				//message.ContinueWhenEditStarted();
-			});
-
-			// 人物編集画面をモーダル表示
-			Messenger.Default.Register<StartEditPersonMessage>((message) =>
-			{
-				throw new NotImplementedException();
-				//view.SubViewClosed += (sender, e) => message.OnEditorClosed();
-				// ここで画面表示
-				//message.ContinueWhenEditStarted();
-			});
-
-			// 場所編集画面をモーダル表示
-			Messenger.Default.Register<StartEditPlaceMessage>((message) =>
-			{
-				throw new NotImplementedException();
-				//view.SubViewClosed += (sender, e) => message.OnEditorClosed();
-				// ここで画面表示
-				//message.ContinueWhenEditStarted();
-			});
-		}
+            LoadApplication(new StoryCanvas.XF.App());
+        }
     }
 }
---
+++ StoryCanvas.XF2/View/Extension/TranslateExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Resources;
+using System.Globalization;
+using System.Reflection;
+using StoryCanvas.Shared.Common;
+
+namespace StoryCanvas.XF.View.Extension
+{
+	public interface ILocalize
+	{
+		CultureInfo GetCurrentCultureInfo();
+
+		void SetLocale();
+	}
+
+	// You exclude the 'Extension' suffix when using in Xaml markup
+	[ContentProperty("Text")]
+	public class TranslateExtension : IMarkupExtension
+	{
+		public string Text { get; set; }
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (Text == null) return null;
+			/*
+			foreach (var fi in typeof(AppResources).GetRuntimeProperties())
+			{
+				if (fi.Name == Text)
+				{
+					return fi.GetValue(null);
+				}
+			}
+			return "Translate Error";
+			*/
+			return StringResourceResolver.Resolve(this.Text);
+		}
+	}
+}
