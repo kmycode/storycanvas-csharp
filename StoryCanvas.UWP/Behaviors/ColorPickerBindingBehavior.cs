21160b34a60024c0d0926f247d7b9474e8ff324c
KMY <tt@kmycode.net>
2017-10-29 15:55:45 +0900

    [UWP] 人物の色選択フライアウトを作成

---
+++ StoryCanvas.UWP/Behaviors/ColorPickerBindingBehavior.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace StoryCanvas.UWP.Behaviors
+{
+    /// <summary>
+    /// ColorPickerのColorプロパティをバインディングしようとしても、
+    /// ValueConverterのConvertBackメソッドには、なぜかColorではなく__ComObjectがvalueとして入ってくるので
+    /// 代替としてビヘイビアを作成。UWPのバグであってほしい
+    /// </summary>
+    class ColorPickerBindingBehavior : Behavior<ColorPicker>
+    {
+        private ColorPicker lastAttached;
+
+        /// <summary>
+        /// 色
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(
+                nameof(Color),
+                typeof(Color),
+                typeof(ColorPickerBindingBehavior),
+                new PropertyMetadata(Colors.Black, OnColorChanged));
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        private static void OnColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var b = (ColorPickerBindingBehavior)d;
+            if (b.AssociatedObject == null) return;
+
+            var color = (Color)e.NewValue;
+            if (color != b.AssociatedObject.Color)
+            {
+                b.AssociatedObject.Color = color;
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.DoDetach();
+            this.AssociatedObject.Color = this.Color;
+            this.AssociatedObject.ColorChanged += this.AssociatedObject_ColorChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.DoDetach();
+        }
+
+        private void DoDetach()
+        {
+            if (this.lastAttached != null)
+            {
+                this.lastAttached.ColorChanged -= this.AssociatedObject_ColorChanged;
+                this.lastAttached = null;
+            }
+        }
+
+        private void AssociatedObject_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+        {
+            this.Color = this.AssociatedObject.Color;
+        }
+    }
+}
---
+++ StoryCanvas.UWP/Converters/ColorResource2ColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using StoryCanvas.Shared.ViewTools;
+using StoryCanvas.Shared.ViewTools.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace StoryCanvas.UWP.Converters
+{
+    class ColorResource2ColorConverter : ValueConverterBase<ColorResource, Color>
+    {
+        public ColorResource2ColorConverter()
+        {
+            this.IsCheckTargetType = false;
+        }
+
+        public override Color Convert(ColorResource value)
+        {
+            return new Color
+            {
+                R = value.R,
+                G = value.G,
+                B = value.B,
+                A = 255,
+            };
+        }
+
+        public override ColorResource ConvertBack(Color value)
+        {
+            return new ColorResource
+            {
+                R = value.R,
+                G = value.G,
+                B = value.B,
+            };
+        }
+    }
+}
